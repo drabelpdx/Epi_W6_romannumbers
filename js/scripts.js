0a1b3ebab4623aad529232f1e4f121fa17e78f8c
jocelyn alsdorf & david abel <student@epicodus.com>
2015-05-28 11:01:41 -0700

    adds function and testing for roman numeral converter

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var romanNumGen = function(number) {
+  var romanConversions = {
+    M:1000,
+    CM:900,
+    D:500,
+    CD:400,
+    C:100,
+    XC:90,
+    L:50,
+    XL:40,
+    X:10,
+    IX:9,
+    V:5,
+    IV:4,
+    I:1
+  }
+  var romanized = "";
+
+    for(var prop in romanConversions) {
+      while(number >= romanConversions[prop]) {
+        romanized += prop;
+        number -= romanConversions[prop];
+      }
+
+   }
+   return romanized;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,10 @@
-describe('', function() {
-  it("", function() {
-    expect(()).to.equal();
+ describe('romanNumGen', function() {
+//   it("will return the value for the given property", function() {
+//     expect(romanNumGen("V")).to.equal(5);
+//   });
+
+  it("will retun a roman numeral for the number entered", function() {
+    expect(romanNumGen(5)).to.equal("V");
   });
 
 });
